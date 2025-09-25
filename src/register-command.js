8a2acab9598ee232cd8af597b324579583ea0eb5
Kim <kimpoychin131@gmail.com>
2025-09-26 01:00:19 +0800

    initialize commit

--- src/register-command.js
+++ src/register-command.js
@@ -63,22 +63,5 @@ const rest = new REST({version:'10'}).setToken(process.env.TOKEN);
 
 
 
-/* const commands = [{
-    name: 'bot',
-    description: "When you say 'yu', he say bot"}]
-
-const rest = new REST ({version: '10'}).setToken(process.env.TOKEN);
-
-(async() =>{
-    try{
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.BOT_ID, process.env.GUILD_ID),
-            {body : commands}
-        );
-
-    }catch(e){
-        console.log(`eror in ${e}`)
-    }
- })(); */
 
  
